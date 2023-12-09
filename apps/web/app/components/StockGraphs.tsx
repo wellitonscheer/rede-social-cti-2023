2aa021a75185c468ec198fe3ce0a7a9ae07de5e4
wellitonscheer <wellitonscheer@gmail.com>
2023-12-08 21:13:35 -0300

    feat: get graphs

--- apps/web/app/components/StockGraphs.tsx
+++ apps/web/app/components/StockGraphs.tsx
@@ -1,28 +1,42 @@
-"use client"
+"use client";
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from "react";
+
+type StockGraph = {
+  url: string;
+  size: number;
+  cors: string;
+  etag: string;
+  expireAt: string;
+  createdAt: string;
+};
 
 const StockGraphs = () => {
+  const [stocks, setStocks] = useState<StockGraph[]>([]);
 
   useEffect(() => {
-
     const buscaStock = async () => {
-      const res = await fetch("/api/stock-graphs", {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-      const product = await res.json()
-      console.log(product)
+      try {
+        const res = await fetch("/api/stock-graphs", {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        const stock: { stocks: StockGraph[] } = await res.json();
+        console.log(stock.stocks);
+        setStocks(stock.stocks);
+      } catch (e) {
+        console.log("error", e);
+      }
     };
 
     buscaStock();
-  }, [])
+  }, []);
   return (
-    <div className='flex flex-row'>
-      <div> stock graphs</div>
-      <div> stock graphs</div>
-      <div> stock graphs</div>
+    <div className="flex flex-row">
+      {stocks?.map((stock, ind) =>
+        stock.url ? <img alt={`${ind}`} src={stock.url} /> : null,
+      )}
     </div>
   );
 };
